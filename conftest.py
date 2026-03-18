c418b34850d26d6519f464eda69bed2058f57e2a
Lechash <lechash@yandex.ru>
2026-03-18 23:08:13 +0300

    зачистила ненужную строку

--- conftest.py
+++ conftest.py
@@ -2,7 +2,6 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
 from test_data import *
 from locators import *
 
